a5589c4adf4d53511e088b0fc05718800615377e
Krystle <krebates@gmail.com>
2014-02-08 21:39:25 -0500

    updated the messages controller to have the message controller functions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,25 @@
 class MessagesController < InheritedResources::Base
+
+ def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+
+    if @message.save
+      UserMailer.contact_seller_email(@message).deliver
+      redirect_to :back, notice: 'Your email is sent! Thank You!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:subject, :text)
+  end
+
 end
