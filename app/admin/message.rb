f9a4a77a3785db36933adf1f821398e866a0469e
TechFounder <techfounder@outlook.com>
2014-02-09 13:28:06 -0500

    Add column to message so messages will show up in active admin

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -1,17 +1,18 @@
 ActiveAdmin.register Message do
+    permit_params :message
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
-end
+    index do
+      column :message
+      default_actions
+    end
+
+    filter :email
+
+    form do |f|
+      f.inputs "Message Details" do
+        f.input :message
+      end
+      f.actions
+    end
+
+  end
