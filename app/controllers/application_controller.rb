7ded0e1cf6cc86b10431d8774bb3b2ad1c1df064
Dan Madere <danmadere@gmail.com>
2014-02-09 07:56:50 -0500

    Fix issues with strong params on user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
  protected
 
  def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :provider, :uid) }
-   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :provider, :uid) }
+   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :name, :provider, :uid, :password, :password_confirmation) }
+   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :name, :provider, :uid, :password, :password_confirmation, :current_password) }
  end 
+
 end
