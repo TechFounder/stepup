30bcb915deafbb2eea7754342206e69d7a2ab40d
Dan Madere <danmadere@gmail.com>
2014-02-08 20:06:11 -0500

    More work on mentor search

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -2,6 +2,12 @@ class MentorsController < ApplicationController
 	before_filter :authenticate_user!
 
   def search
-  	@mentors = User.mentor
+  	if params[:query]
+  		query = "%" + params[:query] + "%"
+  		@mentors = User.where(['name LIKE ? OR mentor_company_name LIKE ? OR mentor_job_title LIKE ? OR mentor_sector LIKE ?' , query, query, query, query])
+  		@is_search_results = true
+  	else
+  		@mentors = User.mentor
+  	end
   end
 end
