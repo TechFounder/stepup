995f0065d89df3adb255ec1c9b55461dd71aa244
TechFounder <techfounder@outlook.com>
2014-02-08 22:08:14 -0500

    Annotate models

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: mentors
+#
+#  id                         :integer          not null, primary key
+#  company_name               :string(255)
+#  job_title                  :string(255)
+#  sector                     :string(255)
+#  college                    :string(255)
+#  skill_set                  :string(255)
+#  additional_info            :text
+#  internship_available       :boolean
+#  internship_job_title       :string(255)
+#  internship_skill_set       :text
+#  internship_additional_info :text
+#  user_id                    :integer
+#  image_file_name            :string(255)
+#  image_content_type         :string(255)
+#  image_file_size            :integer
+#  image_updated_at           :datetime
+#  created_at                 :datetime
+#  updated_at                 :datetime
+#
+
 class Mentor < ActiveRecord::Base
 	belongs_to :user
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  message    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Message < ActiveRecord::Base
 
 belongs_to :user
